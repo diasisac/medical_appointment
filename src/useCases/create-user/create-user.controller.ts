48f5109ed5e029db3b4710b9c0b176625f6805f7
Isac Dias <isac@DESKTOP-96BSIFB.localdomain>
2022-12-30 17:35:34 -0300

    inserindo logs na aplicacao com winston

--- src/useCases/create-user/create-user.controller.ts
+++ src/useCases/create-user/create-user.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express'
+import { logger } from '../../utils/logger'
 import { CreateUserUseCase } from './create-user.usecase'
 
 export class CreateUserController {
   async handle(request: Request, response: Response) {
     try {
+      logger.info('Usuário sendo criado!')
       const data = request.body
 
       const createUser = new CreateUserUseCase()
@@ -11,6 +13,7 @@ export class CreateUserController {
       const user = await createUser.execute(data)
       return response.status(201).json(user)
     } catch (error: any) {
+      logger.error(error.stack)
       return response.status(400).json({ error: error.message })
     }
   }
