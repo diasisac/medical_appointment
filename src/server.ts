c7e18408e993692c2cb556c3db344259563b15fe
Isac Dias <isac@DESKTOP-96BSIFB.localdomain>
2023-04-06 12:37:22 -0300

    configurando swagger na aplicacao

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 import { userRouter } from './routes/user.routes'
+import swaggerUI from 'swagger-ui-express'
+import swaggerDocument from '../swagger.json'
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.use(userRouter)
 
